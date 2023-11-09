135280ae960535d35770e20a0d19a3bf451c1d83
samsularifin05 <samsul026@gmail.com>
2023-11-09 14:16:36 +0700

    update versi

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -24,13 +24,29 @@ firebase.initializeApp(firebaseConfig);
 // Retrieve firebase messaging
 const messaging = firebase.messaging();
 
-messaging.onBackgroundMessage(function (payload) {
-  console.log("Received background message ", payload);
-
+messaging.onMessage((payload) => {
+  console.log("TESTSTSTST ", payload.notification.body);
   const notificationTitle = payload.notification.title;
   const notificationOptions = {
     body: payload.notification.body
   };
 
-  self.registration.showNotification(notificationTitle, notificationOptions);
+  return self.registration.showNotification(
+    notificationTitle,
+    notificationOptions
+  );
+});
+
+messaging.onBackgroundMessage(function (payload) {
+  console.log("Received background message ", payload);
+  // Customize notification here
+  const notificationTitle = payload.notification.title;
+  const notificationOptions = {
+    body: payload.notification.body
+  };
+
+  return self.registration.showNotification(
+    notificationTitle,
+    notificationOptions
+  );
 });
--- src/App.jsx
+++ src/App.jsx
@@ -1,50 +1,54 @@
-// import { useEffect, useState } from "react";
-// import firebase from "firebase/app";
-// import "firebase/messaging";
+import { useEffect, useState } from "react";
+import firebase from "firebase/app";
+import "firebase/messaging";
 
-// const firebaseConfig = {
-//   apiKey: "AIzaSyC7zs88hUVN6lej2m2iwWHg0rlzIMuGGoI",
-//   authDomain: "sagayaku-cfb0c.firebaseapp.com",
-//   databaseURL: "https://sagayaku-cfb0c-default-rtdb.firebaseio.com",
-//   projectId: "sagayaku-cfb0c",
-//   storageBucket: "sagayaku-cfb0c.appspot.com",
-//   messagingSenderId: "446790869852",
-//   appId: "1:446790869852:web:fee7e7b4ea13f96aaa9799",
-//   measurementId: "G-NH13DE5WMB"
-// };
+const firebaseConfig = {
+  apiKey: "AIzaSyC7zs88hUVN6lej2m2iwWHg0rlzIMuGGoI",
+  authDomain: "sagayaku-cfb0c.firebaseapp.com",
+  databaseURL: "https://sagayaku-cfb0c-default-rtdb.firebaseio.com",
+  projectId: "sagayaku-cfb0c",
+  storageBucket: "sagayaku-cfb0c.appspot.com",
+  messagingSenderId: "446790869852",
+  appId: "1:446790869852:web:fee7e7b4ea13f96aaa9799",
+  measurementId: "G-NH13DE5WMB"
+};
 
-// firebase.initializeApp(firebaseConfig);
+if (!firebase.apps.length) {
+  firebase.initializeApp(firebaseConfig);
+}
 
-// const messaging = firebase.messaging();
+const messaging = firebase.messaging();
 
 function App() {
   // const [notification, setNotification] = useState(null);
 
-  // useEffect(() => {
-  //   // Request notification permission
-  //   messaging.requestPermission().then(() => {
-  //     // Get the registration token
-  //     messaging.getToken().then((token) => {
-  //       console.log("FCM token:", token);
-  //       // Send the token to your server to store it for later use
-  //     });
-  //   });
+  const [tokens, settoken] = useState("");
+  useEffect(() => {
+    messaging.requestPermission().then(() => {
+      // Get the registration token
+      messaging.getToken().then((token) => {
+        console.log(token);
+        settoken(token);
 
-  //   // // Subscribe to a topic
-  //   // messaging.subscribe("general").then(() => {
-  //   //   console.log("Subscribed to topic: general");
-  //   // });
+        // Send the token to your server to store it for later use
+      });
+      messaging.onMessage((payload) => {
+        console.log("Received message:", payload.notification);
+      });
+    });
+    // // Subscribe to a topic
+    // messaging.subscribe("news").then((payload) => {
+    //   console.log(payload);
+    //   console.log("Subscribed to topic: general");
+    // });
 
-  //   // Handle incoming messages
-  //   messaging.onMessage((payload) => {
-  //     console.log("Received message:", payload);
-  //     setNotification(payload.data.notification);
-  //   });
-  // }, []);
+    // Handle incoming messages
+  }, []);
 
+  // console.log(notification);
   return (
     <div>
-      asdas
+      {tokens}
       {/* {notification && (
         <div>
           <p>Title: {notification.title}</p>
