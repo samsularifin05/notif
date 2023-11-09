435114522f86527c065bf15b21e57eda10e9c89e
samsularifin05 <samsul026@gmail.com>
2023-11-09 14:52:15 +0700

    update

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,22 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", () => {
+    navigator.serviceWorker
+      .register(`/serviceWorker.js`)
+      .then((registration) => {
+        console.log(
+          "Service Worker registered with scope:",
+          registration.scope
+        );
+      })
+      .catch((error) => {
+        console.error("Error registering Service Worker:", error);
+      });
+  });
+}
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
