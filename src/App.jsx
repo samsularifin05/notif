c6f8cc872a8c8b13b7c4b625550330ebd91873eb
samsularifin05 <samsul026@gmail.com>
2023-11-09 09:57:43 +0700

    update fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,56 +1,56 @@
-import { useEffect, useState } from "react";
-import firebase from "firebase/app";
-import "firebase/messaging";
+// import { useEffect, useState } from "react";
+// import firebase from "firebase/app";
+// import "firebase/messaging";
 
-const firebaseConfig = {
-  apiKey: "AIzaSyC7zs88hUVN6lej2m2iwWHg0rlzIMuGGoI",
-  authDomain: "sagayaku-cfb0c.firebaseapp.com",
-  databaseURL: "https://sagayaku-cfb0c-default-rtdb.firebaseio.com",
-  projectId: "sagayaku-cfb0c",
-  storageBucket: "sagayaku-cfb0c.appspot.com",
-  messagingSenderId: "446790869852",
-  appId: "1:446790869852:web:fee7e7b4ea13f96aaa9799",
-  measurementId: "G-NH13DE5WMB"
-};
+// const firebaseConfig = {
+//   apiKey: "AIzaSyC7zs88hUVN6lej2m2iwWHg0rlzIMuGGoI",
+//   authDomain: "sagayaku-cfb0c.firebaseapp.com",
+//   databaseURL: "https://sagayaku-cfb0c-default-rtdb.firebaseio.com",
+//   projectId: "sagayaku-cfb0c",
+//   storageBucket: "sagayaku-cfb0c.appspot.com",
+//   messagingSenderId: "446790869852",
+//   appId: "1:446790869852:web:fee7e7b4ea13f96aaa9799",
+//   measurementId: "G-NH13DE5WMB"
+// };
 
-firebase.initializeApp(firebaseConfig);
+// firebase.initializeApp(firebaseConfig);
 
-const messaging = firebase.messaging();
+// const messaging = firebase.messaging();
 
 function App() {
-  const [notification, setNotification] = useState(null);
+  // const [notification, setNotification] = useState(null);
 
-  useEffect(() => {
-    // Request notification permission
-    messaging.requestPermission().then(() => {
-      // Get the registration token
-      messaging.getToken().then((token) => {
-        console.log("FCM token:", token);
-        // Send the token to your server to store it for later use
-      });
-    });
+  // useEffect(() => {
+  //   // Request notification permission
+  //   messaging.requestPermission().then(() => {
+  //     // Get the registration token
+  //     messaging.getToken().then((token) => {
+  //       console.log("FCM token:", token);
+  //       // Send the token to your server to store it for later use
+  //     });
+  //   });
 
-    // // Subscribe to a topic
-    // messaging.subscribe("general").then(() => {
-    //   console.log("Subscribed to topic: general");
-    // });
+  //   // // Subscribe to a topic
+  //   // messaging.subscribe("general").then(() => {
+  //   //   console.log("Subscribed to topic: general");
+  //   // });
 
-    // Handle incoming messages
-    messaging.onMessage((payload) => {
-      console.log("Received message:", payload);
-      setNotification(payload.data.notification);
-    });
-  }, []);
+  //   // Handle incoming messages
+  //   messaging.onMessage((payload) => {
+  //     console.log("Received message:", payload);
+  //     setNotification(payload.data.notification);
+  //   });
+  // }, []);
 
   return (
     <div>
       asdas
-      {notification && (
+      {/* {notification && (
         <div>
           <p>Title: {notification.title}</p>
           <p>Message: {notification.body}</p>
         </div>
-      )}
+      )} */}
     </div>
   );
 }
